bada7575c2f770f58c4da7aa13be35159a389b42
Jean Cyr <jean.m.cyr@gmail.com>
2018-05-08 20:01:29 -0400

    Implement built-in http server for miner state reports

---
+++ libapicore/httpServer.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <thread>
+#include <libethcore/Farm.h>
+
+class httpServer
+{
+public:
+    httpServer();
+    ~httpServer();
+    void run(unsigned short port, dev::eth::Farm* farm);
+    void run_thread();
+    void getstat1(stringstream& ss);
+
+    dev::eth::Farm* m_farm;
+    std::string m_port;
+private:
+    void tableHeader(stringstream& ss, unsigned columns);
+};
+
+extern httpServer http_server;
+
